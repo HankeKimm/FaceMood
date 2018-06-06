c100ead3daf4ea474fbf04401705b624bf399d1e
Hanke Kimm <hankekimm@Hankes-MacBook-Pro.local>
2018-06-06 13:15:53 +0300

    Updated the Aware Service

--- app/src/main/java/com/socialtracking/ubiss/MainService.java
+++ app/src/main/java/com/socialtracking/ubiss/MainService.java
@@ -1,10 +1,14 @@
 package com.socialtracking.ubiss;
 
 import android.app.Service;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.IBinder;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
+import com.aware.Applications;
 import com.aware.Aware;
 
 public class MainService extends Service {
@@ -18,8 +22,41 @@ public class MainService extends Service {
 
     @Override
     public void onCreate() {
-        android.os.Debug.waitForDebugger();
         Intent aware = new Intent(this, Aware.class);
         startService(aware);
+
+        Applications.isAccessibilityServiceActive(this);
+
+        Applications.setSensorObserver(new Applications.AWARESensorObserver() {
+            @Override
+            public void onForeground(ContentValues contentValues) {
+                System.out.println(contentValues.describeContents());
+            }
+
+            @Override
+            public void onNotification(ContentValues contentValues) {
+
+            }
+
+            @Override
+            public void onCrash(ContentValues contentValues) {
+
+            }
+
+            @Override
+            public void onKeyboard(ContentValues contentValues) {
+
+            }
+
+            @Override
+            public void onBackground(ContentValues contentValues) {
+
+            }
+
+            @Override
+            public void onTouch(ContentValues contentValues) {
+
+            }
+        });
         }
 }
