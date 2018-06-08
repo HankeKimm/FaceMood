836813f848205c4b2937fcee81bcb7c9895fab89
Heng Zhang <hengzhang@Hengs-MacBook-Pro.local>
2018-06-08 10:59:00 +0300

    fix some error

--- app/src/main/java/com/socialtracking/ubiss/DataManager.java
+++ app/src/main/java/com/socialtracking/ubiss/DataManager.java
@@ -121,43 +121,5 @@ public class DataManager {
         }
         return facebookUsageList;
     }
-
-
-    public HashMap<Double, Double> retrieveFacebookData2() {
-        Calendar today = Calendar.getInstance();
-        today.set(Calendar.HOUR_OF_DAY,0);
-        today.set(Calendar.MINUTE, 0);
-        today.set(Calendar.SECOND, 0);
-
-        Cursor cursor = context.getContentResolver().query(
-                Applications_Provider.Applications_Foreground.CONTENT_URI, null,
-                Applications_Provider.Applications_Foreground.TIMESTAMP + ">=" +today.getTimeInMillis(), null,
-                Applications_Provider.Applications_Foreground.TIMESTAMP + " ASC");
-
-        HashMap<Double, Double> facebook_usage = new HashMap<>();
-
-        if (cursor != null && cursor.moveToFirst()) {
-
-            double elapsed = 0;
-            double timestamp_start = 0;
-
-            do {
-                if(cursor.getString(cursor.getColumnIndex(Applications_Provider.Applications_Foreground.PACKAGE_NAME)).equals("com.facebook.katana")) {
-                    elapsed = 0;
-                    timestamp_start = cursor.getDouble(cursor.getColumnIndex(Applications_Provider.Applications_Foreground.TIMESTAMP));
-                } else if (timestamp_start > 0 &&
-                        !cursor.getString(cursor.getColumnIndex(Applications_Provider.Applications_Foreground.PACKAGE_NAME)).equals("com.facebook.katana")) {
-                    elapsed = cursor.getDouble(cursor.getColumnIndex(Applications_Provider.Applications_Foreground.TIMESTAMP))-timestamp_start;
-                    facebook_usage.put(timestamp_start, elapsed);
-                    timestamp_start = 0;
-                }
-
-                Log.d("DataManager", timestamp_start + "");
-
-
-            } while (cursor.moveToNext());
-        }
-
-        return facebook_usage;
-    }
+    
 }
